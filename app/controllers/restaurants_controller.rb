b86d90c24b8bcc7955698715dea6d38ec8ba19ae
Alex Hazoury <a.hazoury@gmail.com>
2013-03-14 14:17:52 +0000

    added restaurants controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+  protect_from_forgery
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    if @restaurant.save
+      flash[:success] = "Added a new Restaurant"
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end 
+
+  def show
+    @restaurant = Restaurant.find_by_id(params[:id])
+    if @restaurant.nil?
+      @restaurants = Restaurant.find(:all)
+      render "index"
+    end
+  end
+
+end
