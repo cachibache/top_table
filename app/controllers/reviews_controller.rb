3058deddc708e9e294a5bca1a05f7aa7ea5d2bba
Alex Hazoury <a.hazoury@gmail.com>
2013-03-15 16:30:39 +0000

    added links and review show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,20 @@
 class ReviewsController < ApplicationController
   protect_from_forgery
 
+  # def index
+  #   @review
+  # end
+
+  def create
+    @restaurant = Restaurant.find_by_id(params[:restaurant_id])
+    @review = @restaurant.reviews.build(params[:review])
+    if @review.save
+      redirect_to @restaurant
+    end
+  end 
+
+
+
   def show
     @restaurant = Restaurant.find_by_id(params[:restaurant_id])
     @review = @restaurant.reviews.find(params[:id])
